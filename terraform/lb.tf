39d1808f28d4025b05e049fb0de58cf2b3cc26ba
RobBlumberg <robertblumberg97@gmail.com>
2024-06-04 16:20:08 -0400

    fix(CHB-3371): enable access logs on load balancer

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,3 +1,110 @@
+locals {
+  access_logs_bucket_name = "${var.common_tags.environment}-grafana-alb-access-logs-${var.region}"
+}
+
+resource "aws_kms_key" "aws_lb_logs" {
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key
+  description         = "KMS Key for encrypting grafana alb access logs"
+  key_usage           = "ENCRYPT_DECRYPT"
+  enable_key_rotation = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+        Action = [
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion",
+          "kms:ReplicateKey",
+          "kms:Decrypt*",
+          "kms:Encrypt*",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:TagResource",
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_kms_alias" "aws_lb_logs" {
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_alias
+  name          = "alias/${var.common_tags.environment}-grafana-alb-access-logs-s3-bucket-key"
+  target_key_id = aws_kms_key.aws_alb_access_logs_bucket.id
+}
+
+resource "aws_s3_bucket" "aws_lb_logs" {
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket 
+  bucket        = local.access_logs_bucket_name
+  force_destroy = false
+
+  tags = var.do_backup ? merge(var.common_tags, { "backup-plan" : var.common_tags.environment }) : var.common_tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "aws_lb_logs" {
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_server_side_encryption_configuration
+  bucket = aws_s3_bucket.aws_lb_logs.id
+
+  rule {
+    bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.aws_alb_access_logs_bucket.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "aws_lb_logs" {
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_ownership_controls
+  bucket = aws_s3_bucket.aws_lb_logs.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "aws_lb_logs" {
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_public_access_block
+  bucket = aws_s3_bucket.aws_lb_logs.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "aws_lb_logs" {
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
+  bucket = aws_s3_bucket.aws_lb_logs.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_lb" "grafana" {
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
   name            = "${var.resource_prefix}-grafana"
@@ -7,6 +114,12 @@ resource "aws_lb" "grafana" {
   idle_timeout    = "3600"
 
   enable_deletion_protection = false
+
+  access_logs {
+    bucket  = aws_s3_bucket.aws_lb_logs.id
+    prefix  = "${local.access_logs_bucket_name}/"
+    enabled = true
+  }
 
   tags = var.common_tags
 }
